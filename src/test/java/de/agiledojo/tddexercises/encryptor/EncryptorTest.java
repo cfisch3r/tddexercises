49a14f6e5e3e794eba0956b5f4b58f4ee18bffeb
Christian Fischer <cf@fisch3r.de>
2017-04-29 12:44:54 +0200

    Encryptor test

---
+++ src/test/java/de/agiledojo/tddexercises/encryptor/EncryptorTest.java
@@ -0,0 +1,13 @@
+package de.agiledojo.tddexercises.encryptor;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class EncryptorTest {
+
+    @Test
+    public void test() {
+        fail("test not yet implemented.");
+    }
+}
